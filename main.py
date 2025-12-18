c5e337744f8844143517b64b5f7264262da0000f
Sahil20079 <pradhansahil160@gmail.com>
2025-12-18 12:01:22 +0545

    update

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ app = FastAPI()
 def greet():
     return "welcome to telusko track"
 
-products=[
-    Product(1,"phone","iphone",99,10),
-    Product(2,"laptop","gaming laptop",999,7)
+products = [
+    Product(id=1, name="Phone", description="A smartphone", price=699.99, quantity=50),
+    Product(id=2, name="Laptop", description="A powerful laptop", price=999.99, quantity=30),
+    Product(id=3, name="Pen", description="A blue ink pen", price=1.99, quantity=100),
+    Product(id=4, name="Table", description="A wooden table", price=199.99, quantity=20),
 ]
 
 @app.get("/product")
